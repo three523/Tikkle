43e8b6cc6fc8a55805f1a861be59f5b9d2032497
real-future <future.ver1@gmail.com>
2023-08-16 12:24:34 +0900

    feat/TikkleListPage에서 CreatTikklePage와 TikklePage로 넘어가는 기능 구현

--- Tikkle/Tikkle/TikkleListPage/TikkleListPageViewController.swift
+++ Tikkle/Tikkle/TikkleListPage/TikkleListPageViewController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class TikkleListPageViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
+}
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension TikkleListPageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+}
 
+extension TikkleListPageViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TikkleListTableViewCell
+        cell.cellTitleLabel.text = "cellTitleLabel"
+        cell.cellPercentLabel.text = "40%"
+        
+        return cell
+    }
+    
 }
