888f269a767ac718b0774a46fe3a3b2edfc99805
real-future <future.ver1@gmail.com>
2023-08-17 09:25:55 +0900

    feat/이미지asset, CustomButton 클래스 만들기, TikkleListPage UI완료

--- Tikkle/Tikkle/TikkleListPage/TikkleListTableViewCell.swift
+++ Tikkle/Tikkle/TikkleListPage/TikkleListTableViewCell.swift
@@ -8,31 +8,35 @@
 import UIKit
 
 class TikkleListTableViewCell: UITableViewCell {
-
+    
     
     
     @IBOutlet weak var tikkleImage: UIImageView!
-    @IBOutlet weak var isPrivateImage: UIImageView!
-    @IBOutlet weak var isSharedProjectImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var percentLabel: UILabel!
     @IBOutlet weak var graphImage: UIImageView!
-    
+    @IBOutlet weak var isPrivateButton: UIButton!
+    @IBOutlet weak var isSharedProjectButton: UIButton!
     
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        let privateTrueButton = CustomButton.makePrivateTrueButton()
+        isPrivateButton.addSubview(privateTrueButton)
+        let sharedTrueButton = CustomButton.makeTogetherButton()
+        isSharedProjectButton.addSubview(sharedTrueButton)
     }
     
     
     
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
